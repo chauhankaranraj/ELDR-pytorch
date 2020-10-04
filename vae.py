f29ef0a762f020ec2c48601e4539c20ea099d4db
Karanraj Chauhan <kachau@redhat.com>
2020-10-04 01:13:13 -0400

    Update iris vae arch. Saved model results

--- vae.py
+++ vae.py
@@ -8,18 +8,49 @@ import torch.nn.functional as F
 
 
 class VAE(nn.Module):
-    def __init__(self, input_ndim, output_ndim):
+    def __init__(
+        self,
+        input_ndim,
+        output_ndim,
+        encode_layer_sizes=(128, 64, 32), 
+        decode_layer_sizes=(32, 64, 128),
+    ):
         super(VAE, self).__init__()
 
-        self.fc1 = nn.Linear(input_ndim, 64)
-        self.fc21 = nn.Linear(64, output_ndim)
-        self.fc22 = nn.Linear(64, output_ndim)
-        self.fc3 = nn.Linear(output_ndim, 64)
-        self.fc4 = nn.Linear(64, input_ndim)
+        # create encoding layers
+        self.encode_layers = nn.ModuleList()
+        for li in range(len(encode_layer_sizes)):
+            if li == 0:
+                self.encode_layers.append(nn.Linear(input_ndim, encode_layer_sizes[li]))
+            else:
+                self.encode_layers.append(nn.Linear(encode_layer_sizes[li-1], encode_layer_sizes[li]))
+        self.mu_encode_layer = nn.Linear(encode_layer_sizes[-1], output_ndim)
+        self.logvar_encode_layer = nn.Linear(encode_layer_sizes[-1], output_ndim)
+
+        # create decoding layers
+        self.decode_layers = nn.ModuleList()
+        for li in range(len(decode_layer_sizes)):
+            if li == 0:
+                self.decode_layers.append(nn.Linear(output_ndim, decode_layer_sizes[li]))
+            else:
+                self.decode_layers.append(nn.Linear(decode_layer_sizes[li-1], decode_layer_sizes[li]))
+        self.decode_layers.append(nn.Linear(decode_layer_sizes[-1], input_ndim))
+
+        # self.fc1 = nn.Linear(input_ndim, 64)
+        # self.fc21 = nn.Linear(64, output_ndim)
+        # self.fc22 = nn.Linear(64, output_ndim)
+        # self.fc3 = nn.Linear(output_ndim, 64)
+        # self.fc4 = nn.Linear(64, input_ndim)
 
     def encode(self, x):
-        h1 = F.relu(self.fc1(x))
-        return self.fc21(h1), self.fc22(h1)
+        # h1 = F.relu(self.fc1(x))
+        # return self.fc21(h1), self.fc22(h1)
+        for li,layer in enumerate(self.encode_layers):
+            if li == 0:
+                h = F.elu(layer(x))
+            else:
+                h = F.elu(layer(h))
+        return self.mu_encode_layer(h), self.logvar_encode_layer(h)
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5*logvar)
@@ -27,8 +58,16 @@ class VAE(nn.Module):
         return mu + eps*std
 
     def decode(self, z):
-        h3 = F.relu(self.fc3(z))
-        return torch.sigmoid(self.fc4(h3))
+        # h3 = F.relu(self.fc3(z))
+        # return torch.sigmoid(self.fc4(h3))
+        for li,layer in enumerate(self.decode_layers):
+            if li == 0:
+                h = F.elu(layer(z))
+            elif li == len(self.decode_layers) - 1:
+                h = torch.sigmoid(layer(h))
+            else:
+                h = F.elu(layer(h))
+        return h
 
     def forward(self, x):
         mu, logvar = self.encode(x)
